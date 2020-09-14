6dfe7999f8105993aec73b01ce2aece450f5d70e
scb3 <scott.c.brooks@outlook.com>
2020-09-14 11:33:25 +0100

    Javascript Numbers and Operators

--- Javascript Stuff/Javascript/Learn_code.js
+++ Javascript Stuff/Javascript/Learn_code.js
@@ -5,7 +5,7 @@ var message = "Hi may name is " + name + "and I am " + age + "years old";
  Function 
 //alert(message);*/
 
-var firstName = "John";
+/*var firstName = "John";
 var lastName = "Smith";
 var dateOfBirth = "10/10/2000";
 var age = "19";
@@ -13,6 +13,14 @@ var profileImageURL = "";
 
 var loginWelcomeMessage = "Welcome " + firstName + " " + lastName +  ". Happy " + age + "th Birthday";
 
-/* Developer Logging - Shift+Ctrl+J in browser */
-console.log(loginWelcomeMessage);
+ Developer Logging - Shift+Ctrl+J in browser 
+console.log(loginWelcomeMessage);*/
 
+var sum = 10 + 15;
+var sub = 15 - 10;
+var mul = 10 * 3;
+var mod = 10%3;
+
+var msg = "10/3 = 3 with remainder " + mod;
+var result = 10*((5*5)-10);
+console.log (result);
