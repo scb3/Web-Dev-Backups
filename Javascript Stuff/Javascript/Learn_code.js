a0daa23172d6ea5f40e6a5f0b634f908a0f4afa8
scb3 <scott.c.brooks@outlook.com>
2020-09-14 09:00:14 +0100

    Refactor and Linking Javascript

---
+++ Javascript Stuff/Javascript/Learn_code.js
@@ -0,0 +1,9 @@
+var name = "Jack";
+var age = "23";
+var message = "Hi may name is " + name + "and I am " + age + "years old";
+
+/* Function */
+//alert(message);
+
+/* Developer Logging - Shift+Ctrl+J in browser */
+console.log(message);
