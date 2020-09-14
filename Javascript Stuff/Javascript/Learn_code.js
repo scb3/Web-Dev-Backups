e06e61cd63680b73f4b4e1758f8597d207e35cdd
scb3 <scott.c.brooks@outlook.com>
2020-09-14 09:35:52 +0100

    Javascript Variables And Strings

--- Javascript Stuff/Javascript/Learn_code.js
+++ Javascript Stuff/Javascript/Learn_code.js
@@ -1,9 +1,18 @@
-var name = "Jack";
+/*var name = "Jack";
 var age = "23";
 var message = "Hi may name is " + name + "and I am " + age + "years old";
 
-/* Function */
-//alert(message);
+ Function 
+//alert(message);*/
+
+var firstName = "John";
+var lastName = "Smith";
+var dateOfBirth = "10/10/2000";
+var age = "19";
+var profileImageURL = "";
+
+var loginWelcomeMessage = "Welcome " + firstName + " " + lastName +  ". Happy " + age + "th Birthday";
 
 /* Developer Logging - Shift+Ctrl+J in browser */
-console.log(message);
+console.log(loginWelcomeMessage);
+
