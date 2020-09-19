005bc301aa2034138e8f107f94968e84be919110
scb3 <scott.c.brooks@outlook.com>
2020-09-19 08:53:01 +0100

    Javascript Logical Operators

--- Javascript Stuff/Javascript/Learn_code.js
+++ Javascript Stuff/Javascript/Learn_code.js
@@ -5,7 +5,7 @@ var message = "Hi may name is " + name + "and I am " + age + "years old";
  Function 
 //alert(message);*/
 
-var firstName = "John";
+/*var firstName = "John";
 var lastName = "Smith";
 var dateOfBirth = "10/10/2000";
 var age = "19";
@@ -14,5 +14,23 @@ var profileImageURL = "";
 var loginWelcomeMessage = "Welcome " + firstName + " " + lastName +  ". Happy " + age + "th Birthday";
 
 /* Developer Logging - Shift+Ctrl+J in browser */
-console.log(loginWelcomeMessage);
+/*console.log(loginWelcomeMessage);*/
+
+if(1===1 && 2 === 2 || "joe" === "joe"){
+    
+    console.log("These are both true");
+}
+
+var cat1 = 5;
+var cat2 = 10;
+var cat3 = 1;
+var cat3DisabledHandicap = true;
+
+if ((cat1 > cat2 && cat1> cat3) && !cat3DisabledHandicap){
+    console.log("Cat 1 is the best");
+}else if((cat2>cat1 && cat2>cat3) && !cat3DisabledHandicap){
+    console.log("cat 2 is the best");
+}else if((cat3>cat1 && cat3>cat2) || cat3DisabledHandicap){
+    console.log("Cat 3 is the best");
+}
 
